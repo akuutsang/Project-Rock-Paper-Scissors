dfba0acff5a7ce3fa1b2adaef457c3ad4169c048
Akutsang <mymac@MYs-MacBook-Pro.local>
2022-11-28 12:35:40 +0100

    This new function is to determine when each of the player wins

--- RPS/rps.js
+++ RPS/rps.js
@@ -38,3 +38,27 @@ function playerChoice(){
   function validateInput(choice){
     return choices.includes(choice);
   }  
+
+//  Next is to creat another function to check winner
+
+
+function checkWinner(choiceP, choiceC){
+
+    if (choiceP === choiceC) {
+  return "Tie";
+  }else if(choiceP == "rock" && choiceC == "scissors"){
+   return "player wins, rock smashes scissors!"
+  }else if (choiceP == "scissors" && choiceP == "paper") {
+   return "player wins, scissors cuts paper!" 
+  } else if(choiceP == "paper" && choiceC) {
+   return "player wins, paper covers rock!"
+  }else if(choiceC == "paper" && choiceP == "rock"){
+   return "computer wins, paper covers rock!"
+  }else if(choiceC == "scissors" && choiceP == "paper"){
+   return "computer wins, scissors cuts paper!"
+  }else if(choiceC == "rock" && choiceP == "scissors"){
+   return "computer wins, rock smashes scissors!"
+  }else{
+   return "invalid response"
+  }
+} 
