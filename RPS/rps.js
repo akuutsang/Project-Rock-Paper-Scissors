56bc259c2a1b17e35403ab81809add10405c38eb
Akutsang <mymac@MYs-MacBook-Pro.local>
2022-11-28 12:29:57 +0100

    two functions allows both player and computer to make a choice withing the array

---
+++ RPS/rps.js
@@ -0,0 +1,36 @@
+// first write an a array in a global scope
+const choices = ["rock", "paper", "scissors"];
+
+// secondly, write a function that will wrap us all your game
+
+function game(){
+    // the final code will go in here
+}
+
+// creat a function for computerChoice which will pick at random
+
+function computerChoice(){
+    return choices[Math.floor(Math.random()*choices.length)];
+  }
+
+// creat a function for player choice and make sure it is valid
+
+function playerChoice(){
+    let input = prompt("Type Rock, Paper or Scissors");
+    while (input == null){
+      input = prompt("Type Rock, Paper or Scissors");
+    }
+    input = input.toLowerCase();
+    let check = validateInput(input);
+    while(check == false){
+      input = prompt(
+       "type Rock, Paper or Scissors. Spelling needs to be exact, but capitalization doesnt matter"
+      );
+      while (input == null){
+        input = prompt("type Rock, Paper or Scissors");
+     }
+     input = input.toLowerCase();
+     check = validateInput(input);
+   } 
+   return input; 
+  }
